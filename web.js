57081462234794a1ba679da347cd2e53298846c9
alberto fuentes <pajaro@gmail.com>
2013-07-11 18:48:01 +0200

    NEW: server now shows index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var content = fs.readFileSync("index.html",ecoding='utf8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//  response.send('Hello World!');
+  response.send(content);
+
+
 });
 
 var port = process.env.PORT || 5000;
